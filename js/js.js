a20f7b910def96e271f12d4c186d30050402184e
Alyona git config --global user.email alena_novour@mail.ru <alena_novour@mail.ru>
2024-02-02 12:55:30 +0300

    refactor

--- js/js.js
+++ js/js.js
@@ -1,45 +1,31 @@
 'use strict'
-//Анимация 
-const animItems = document.querySelectorAll('._animation');
-if (animItems.length > 0) {
-   window.addEventListener('scroll', animOnScroll);
-   function animOnScroll () {
-      for (let i=0; i < animItems.length; i++) {
-         const animItem = animItems[i];
-         const animItemHeight = animItem.offsetHeight;
-         const animItemOffset = offset(animItem).top;
-         const animStart = 4;
-         let animItemPoint = window.innerHeight - animItemHeight / animStart;
-         if (animItemHeight > window.innerHeight) {
-            animItemPoint = window.innerHeight - window.innerHeight / animStart;
-         }
-         if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset  + animItemHeight)) {
-            animItem.classList.add('_active');
-         } else {
-            animItem.classList.remove('_active');
-         }
-      }
-}
-
-function offset(el) {
-   const rect = el.getBoundingClientRect(),
-      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-   return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
-}
-setTimeout(animOnScroll(), 3)
-
-}
-
 
 //Бургер-меню
    const menuIcon = document.querySelector('.menu__icon');
+   const headerMenu = document.querySelector('.header__menu');
    if (menuIcon) {
-      const headerMenu = document.querySelector('.header__menu');
+      
       menuIcon.addEventListener('click', function(e) {
-         document.body.classList.toggle('_lock');
-         menuIcon.classList.toggle('_active');
+         e.preventDefault();
          headerMenu.classList.toggle('_active');
       }
       )
    }
+//переход на страницу 
+const menu = document.querySelector('.menu-list');
+menu.addEventListener('click', event => {
+   if (event.target.className === 'click') { 
+      console.log('Click!');
+   }
+});
+//форма отправки 
+
+const form = document.querySelector('.form');
+const submit = form.querySelector('.form__button');
+submit.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      form.innerHTML='Форма отправлена. Свяжемся с вами в ближайшее время:)';
+      setTimeout(function() {
+      form.classList.remove('form--open');;
+   }, 3000);
+})
